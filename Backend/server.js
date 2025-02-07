f25245aea5d3aeddd7ce0be5df9fe074d55e8ecb
Vinay reddy koukuntla <thewallofdreamsvinay@gmail.com>
2025-02-07 23:05:33 +0530

    env
    added env

--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,10 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
-
-
 const bodyParser = require("body-parser");
-
 const personRoutes = require("./Routes/personRoutes");
-
 app.use(bodyParser.json());
-
 app.use("/", personRoutes);
-
 app.listen(process.env.PORT, () =>
   console.log("Server is running on port 6001")
 );
---
+++ Frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Home from './pages/Home'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/about" element={<About />} /> */}
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
