121eb1ecb0ad432ef236bf09e1f023489c81bc98
Abhinaba Das <tstatai02@gmail.com>
2025-03-17 13:30:58 +0530

    fixed the courseDetails page error in both local and deploy server

--- Frontend/src/pages/course/CourseDetailsPage.jsx
+++ Frontend/src/pages/course/CourseDetailsPage.jsx
@@ -303,7 +303,7 @@ const CourseDetailsPage = () => {
                 </section>
               )}
 
-              <div className="w-full max-w-[400px] h-[250px] overflow-hidden rounded-md shadow-md">
+              <div className="w-full max-w-[500px] h-[300px] overflow-hidden rounded-md shadow-md">
                 <img
                   src={`https://twod-tutorial-web-application-3brq.onrender.com${course.nameImage}`}
                   alt={course.nameImage}
