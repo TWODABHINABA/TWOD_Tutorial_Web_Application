a9f595356fe794ad490b18ee8d58d74189c1fe65
Abhinaba Das <tstatai02@gmail.com>
2025-03-17 16:02:19 +0530

    fixed the courseDetails page error in both local and deploy server

--- Frontend/src/pages/sessionTiming/SessionAdmin.jsx
+++ Frontend/src/pages/sessionTiming/SessionAdmin.jsx
@@ -107,7 +107,7 @@ const SessionAdmin = () => {
             className="border rounded-lg px-3 py-2 w-1/2"
           />
           <input
-            type="number"
+            type="text"
             placeholder="Price (e.g., 2200)"
             value={newPrice}
             onChange={(e) => setNewPrice(e.target.value)}
