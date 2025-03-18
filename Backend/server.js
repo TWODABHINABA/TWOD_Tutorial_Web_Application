8f05a01f832d43fa6fc1cb4c86b6fa4d9f6afe23
Abhinaba Das <tstatai02@gmail.com>
2025-03-18 13:41:26 +0530

    fixed the courseDetails page error in both local and deploy server

--- Backend/server.js
+++ Backend/server.js
@@ -11,25 +11,8 @@ const path = require('path');
 // const paypalRoutes = require('./Routes/paypal');
 app.use(express.json());
 
-// app.use(cors({ origin: "http://localhost:5173", credentials: true })); //Local
+app.use(cors({ origin: "http://localhost:5173", credentials: true })); //Local
 // app.use(cors({ origin: "https://twod-tutorial-web-application-frontend.vercel.app", credentials: true })); //Abhi
-const allowedOrigins = [
-  "http://localhost:5173", 
-  // "https://twod-tutorial-web-application-frontend.vercel.app"
-];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -47,26 +30,7 @@ app.listen(process.env.PORT, () =>
 
 
 
-// const allowedOrigins = [
-//   'http://localhost:5173', // Local dev
-//   // 'https://twod-tutorial-web-application.vercel.app', // Production frontend
-//   // 'https://twod-tutorial-web-application-tn27xfrr3.vercel.app',
-//   // 'https://twod-tutorial-web-applicati-git-2ee84e-vinays-projects-73cfc7f5.vercel.app'
-//   // "https://twod-tutorial-web-application-frontend.vercel.app",
-//   // "https://twod-tutorial-web-application.vercel.app/"
-// ];
-// app.use(cors({
-//   origin: function (origin, callback) {
-//     if (!origin || allowedOrigins.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-//   credentials: true,
-//   // methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   // allowedHeaders: ['Content-Type', 'Authorization'], 
-// }));
+
 // const corsOptions = {
 //   origin: function (origin, callback) {
 //     if (!origin || allowedOrigins.includes(origin)) {
